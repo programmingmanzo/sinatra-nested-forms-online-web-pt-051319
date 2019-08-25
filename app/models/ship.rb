6cabd95639974967fb6e4ac28b81b24a731bf12c
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-25 22:21:42 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_reader :name, :type, :booty 
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(details)
+        @name = details[:name]
+        @type = details[:type]
+        @booty = details[:booty]
+        @@all << self
+    end
+
+    def self.clear 
+        all.clear
+    end
 end
